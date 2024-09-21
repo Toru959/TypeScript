a5cb3f33f37e691c33ea3e93ce365fb959415e50
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-21 11:54:15 +0800

    boolean,number,string

---
+++ section01/index.ts
@@ -0,0 +1,12 @@
+// boolean
+let hasValue: boolean = true;
+
+// number
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+
+// string
+let single: string = 'Hello';
+let double: string = "Hello";
+let back: string = `Hello`; 

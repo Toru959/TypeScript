8ba0d9097c405ebbe714b02ce6b761ec542db077
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-22 09:43:52 +0800

    object,array,tuple,enum,any

---
+++ section01/index.js
@@ -0,0 +1,19 @@
+// boolean
+var hasValue = true;
+// number
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+// string
+var single = 'Hello';
+var double = "Hello";
+var back = "Hello";
+//オブジェクトに型を付ける
+var person = {
+    name: 'Jack',
+    age: 21
+};
+console.log(person.name);
+// 配列
+var fruits = ['Apple', 'Banana', 'Grape'];
+var fruits2 = ['Apple', 'Banana', 'Grape', 1, 2];
--- section01/index.ts
+++ section01/index.ts
@@ -10,3 +10,50 @@ let negative: number = -0.12;
 let single: string = 'Hello';
 let double: string = "Hello";
 let back: string = `Hello`; 
+
+//オブジェクトに型を付ける
+const person: {
+    name: string,
+    age: number
+} = {
+    name: 'Jack',
+    age: 21
+}
+
+console.log(person.name);
+
+// 配列
+const fruits: string[] = ['Apple', 'Banana', 'Grape'];
+const fruits2: any[] = ['Apple', 'Banana', 'Grape', 1, 2];
+
+// 型注釈、型推論
+// name: string. age:number (型注釈)明示的
+// name: 'Jack' age = 21 (型推論)明示的に型を提唱しなくても勝手に判断される
+
+// Tuple型 決まった内容の配列を作る
+const book: [string, number, boolean] = ['manga', 500, false];
+
+// Enum 列挙型 (特定のまとまったグループのみを受け入れる)
+enum CoffeeSize {
+    SHORT = 'SHORT',
+    TALL = 'TALL',
+    GRANDE = 'GRANDE',
+    VENTI = 'VENTI'
+}
+
+const coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+}
+
+coffee.size = CoffeeSize.GRANDE;
+
+// any型
+let anything: any = true;
+anything = 'hello';
+anything = ['hello', 123, true];
+anything = {};
+anything.abc = 'abc';
+let banana = 'banana';
+banana = anything;
+

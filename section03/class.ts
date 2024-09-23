e2d4adcc821f5d3d1b7be52afc2696a8b518548b
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-24 01:09:01 +0800

    readonly

--- section03/class.ts
+++ section03/class.ts
@@ -1,11 +1,19 @@
 class Person {
 
-    constructor(public name: string, private age: number){
-       
+    readonly id: number = 1;
+
+    // privateはクラス内では読み書きできる。
+    // readonlyはクラス内外でも読み取りだけ
+    constructor(public readonly name: string, private age: number){
+        this.name = "Tom"
+        this.age = 31;
+        this.id = 2;
+        this.id = 3;
     }
 
     incrementAge(){
         this.age += 1;
+        // this.id = 2;
     }
 
     greeting(this:Person){
@@ -14,10 +22,17 @@ class Person {
     }
 }
 
-
+let person2 = Person;
 const Json = new Person('Json', 21);
 
+let UserName = Json.name;
+console.log(UserName);
+//一回変数に入れれば書き換えできる
+UserName = 'Tom';
+console.log(UserName);
+
 Json.incrementAge();
+console.log(Json.id);
 // 直接代入できてしまう
 // Json.age = 42;
 Json.greeting();

e44b22fc268653cb5f9a6d4dc8d7390588866beb
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-25 10:03:08 +0800

    構造的部分型

--- section04/interface.ts
+++ section04/interface.ts
@@ -1,15 +1,25 @@
 //　オブジェクトのみのタイプエイリアス
 // 変数のようにオブジェクトをまとめることで、使い回す時に便利
 interface Human {
-    name: string;
-    age: number;
-    // greeting: (message: string) => void; 関数の型定義
-    greeting(message: string): void;
+   name: string,
+   age: number,
+   greeting(message: string): void,
 }
 
 class Developer implements Human{
     constructor(public name: string, public age: number, public experience: number){}  
     greeting(message: string) {
-        console.log('Hello!');
+        console.log(message);
     }  
 }
+
+const temDeveloper = {
+    name: 'Jeff',
+    age: 32,
+    experience: 4,
+    // greeting: (message: string) => void; 関数の型定義
+    greeting(message: string){
+        console.log(message)
+    }
+}
+const user: Human = temDeveloper;

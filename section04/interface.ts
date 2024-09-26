ddd90b87a4872382087429268bfa071a4df039aa
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-26 09:38:11 +0800

    extends interface

--- section04/interface.ts
+++ section04/interface.ts
@@ -1,12 +1,17 @@
+interface Nameable {
+    name: string;
+}
+
 //　オブジェクトのみのタイプエイリアス
 // 変数のようにオブジェクトをまとめることで、使い回す時に便利
-interface Human {
+// interfaceを継承 interfaceの場合いくつも継承できる（classは１つだけ）
+interface Human extends Nameable {
    readonly name: string,
    age: number,
    greeting(message: string): void,
 }
 
-class Developer implements Human{
+class Developer implements Human, Nameable {
     constructor(public name: string, public age: number, public experience: number){}  
     greeting(message: string) {
         console.log(message);
@@ -22,7 +27,6 @@ const temDeveloper = {
         console.log(message)
     }
 }
-
 const user: Human = temDeveloper;
 let developer = new Developer('Mark', 23, 5);
-developer.name = 'Tom';
+developer.name = 'Tom'

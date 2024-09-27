823fdd46e1dd488c07aeb7fc118e44a4f122167a
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-27 08:39:09 +0800

    3 type guard

--- section05/advanced.js
+++ section05/advanced.js
@@ -4,3 +4,41 @@ const John = {
     role: 'front-end',
     follower: 200
 };
+// 型を絞り込む方法　タイプガード
+//typeof
+function toUpperCase(x) {
+    if (typeof x === 'string') {
+        return x.toUpperCase();
+    }
+    return '';
+}
+function describeProfile(nomadWorker) {
+    console.log(nomadWorker.name);
+    if ('role' in nomadWorker) {
+        console.log(nomadWorker.role);
+    }
+    if ('follower' in nomadWorker) {
+        console.log(nomadWorker.follower);
+    }
+}
+class Dog {
+    speak() {
+        console.log('bow-bow');
+    }
+}
+class Bird {
+    speak() {
+        console.log('tweet-tweet');
+    }
+    fly() {
+        console.log('flutter');
+    }
+}
+function havePet(pet) {
+    pet.speak();
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+havePet(new Bird());
+havePet(new Dog());
--- section05/advanced.ts
+++ section05/advanced.ts
@@ -34,3 +34,53 @@ interface EngineerBlogger2 extends Engineer2, Blogger2 {
 type NumberBoolean = number | boolean;
 type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber;
+
+
+// 型を絞り込む方法　タイプガード
+//typeof
+function toUpperCase(x: string | number){
+    if (typeof x === 'string'){
+        return x.toUpperCase();
+    } 
+
+    return '';
+}
+
+// in演算子
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker){
+    console.log(nomadWorker.name);
+    if ('role' in nomadWorker){
+        console.log(nomadWorker.role);
+    }
+    if('follower' in nomadWorker){
+        console.log(nomadWorker.follower);
+    }
+}
+
+class Dog {
+    speak(){
+        console.log('bow-bow');
+    }
+}
+
+class Bird {
+    speak(){
+        console.log('tweet-tweet');
+    }
+    fly(){
+        console.log('flutter');
+    }
+}
+
+// instanceof
+type Pet = Dog | Bird;
+function havePet(pet: Pet){
+    pet.speak();
+    if (pet instanceof Bird){
+        pet.fly();
+    }
+}
+
+havePet(new Bird());
+havePet(new Dog());

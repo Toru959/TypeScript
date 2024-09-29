d137b0a31027f5a0f02f0c91cf118f072594ea94
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-29 14:46:17 +0800

    hoisting

---
+++ section00/hoisting.ts
@@ -0,0 +1,14 @@
+// 関数宣言と巻き上げ(hoisting)
+// 関数宣言には巻き上げがあり、関数式には巻き上げがない。
+
+//  関数の定義前で関数を呼び出しているのにエラーにならない。
+// hello();
+// function hello(){
+//     console.log("Hello World!");
+// };
+
+//　エラーになる
+// hello();
+// const hello = function(){
+//     console.log("Hello World!");
+// }

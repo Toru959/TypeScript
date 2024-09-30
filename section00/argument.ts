c420f6bba98407089ed2eb5fad7e0d0a46e04480
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-10-01 04:54:12 +0800

    オプション引数

--- section00/argument.ts
+++ section00/argument.ts
@@ -60,3 +60,48 @@
 // change(x);
 // console.log(x); // { n: 1 }
 
+'オブション引数'
+'オブション引数とは、渡す引数を省略できるようにするTypeScript固有の機能です。オブション引数は疑問符?を引数名の後ろに書くことで表現します。'
+// function hello(person?: string){}
+// hello(); // 引数を省略して呼び出せる
+// hello("alice"); // 省略しない呼び出しももちろんOK
+
+'オブション引数の型は、型とundefinedのユニオン型になります。ユニオン型は日本語で言うと「いずれか」の意味です。'
+'引数を省略し他場合、オプション引数の実行時の値はundefinedになります。'
+
+
+'オブション引数の取り回し'
+'オブション引数は、型が、undefinedとユニオン型になるため、そのままでは使用できない。'
+// function hello(person?: string){
+//     return "Hello" + person?.toUpperCase(); 
+// }
+
+'デフォルト値を代入する'
+// function hello(person?: string){
+//     if ( typeof person === "undefined"){
+//         person = "anonymous";
+//     }
+//     return "Hello" + person.toUpperCase();
+// }
+
+'Null合体代入演算子??= でデフォルト値を代入する方法もある'
+// function hello(person?: string){
+//     person ??= "anonymous";
+//     return "Hello" + person.toUpperCase(); 
+// }
+
+'デフォルト引数を指定することでも同じことができる。多くのケースではデフォルト引数を使う方がいい'
+// function hello(person: string = "anonymous"){
+//     return "Hello" + person.toUpperCase();
+// }
+
+
+'T | undefinedとの違い'
+'オブション引数はユニオン型T | undefinedとして解釈されます。であれば、引数の型をT | undefinedとかけば同じなはず。なぜ?を使用するのか'
+'これには呼び出す側で、引数を省略できるかどうかという違いが生まれます。オプション引数は引数自体を省略できますが、T | undefinedは省略できない'
+// function hello(person: string | undefined){}
+// hello(); // 引数がないためコンパイルエラー
+
+'オプション引数の後に普通の引数は書けない'
+'オプション引数は必ず最後に書かないといけない。コンパイルエラーになる'
+// function func(foo?: string, bar: string)

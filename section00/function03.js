f3adf4a8218fe722d5846340a89bf8fbb982f760
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-10-02 19:12:51 +0800

    即時実行関数式

--- section00/function03.js
+++ section00/function03.js
@@ -23,3 +23,53 @@
 // animal.quacks();
 // isDuck(animal);
 // animal.quacks();
+'即時実行関数式';
+'IIFEとは定義と同時に実行される関数です。デザインパターンの1種で、Self-Executing Anonymous Function; 自己実行匿名関数とも呼ばれる';
+// (() => {
+//     console.log("IIFE");
+// })();
+// const result1 = (function(arg: string){
+//     console.log(`IIFE with args:${arg}`);
+//     return "IIFE with args";
+// })("hoge");
+// const result2 = await(async () => {
+//     console.log("async IIFE");
+//     return "async IIFE";
+// })();'
+'TypeScriptでの利用シーン';
+'ReactのuseEffectなど、非同期関数を受け取らない引数に非同期処理を渡したい場合';
+'ReactのuseEffectなど、非同期関数を受け取らない引数に非同期処理を渡したい場合、即時実行関数を使用することで、非同期処理を弾きうすに渡すことができる';
+// useEffect(() => {
+//     (async () => {
+//         const result = await fetch("https://hoge.com");
+//         console.log(result);
+//     })();
+// }, []);
+'ifやswitchなどを式として扱いたい場合';
+'TypeScriptでのifやswitchは構文であり式でないため、判定結果を変数に代入することができせん。そのため、擬似的にifやswitchを式として扱うときにIIFEを使用できます。';
+// const result = ((type: string) => {
+//     if (type === "Apple"){
+//         return "リンゴ";
+//     } else if (type === "Orange"){
+//         return "オレンジ";
+//     } else {
+//         return "謎の果物";
+//     }
+// })(fruit.type);
+'スコープ内での変数汚染を防ぐ';
+'汎用的な変数の場合、同じスコープ内で複数回使いまわしたい場合があるかと思います。';
+'その際に、IIFEを利用することで変数名のスコープを限定し名前の重複を回避できます。';
+// async function callApiAAndB(){
+//     await (async() => {
+//         const result = await fetch("api1");
+//         if(result !== "OK"){
+//             console.log(result);
+//         }
+//     })();
+//     await (async () => {
+//         const result = await fetch("api2");
+//         if(result !== "Success"){
+//             console.log(result);
+//         }
+//     })();
+// }

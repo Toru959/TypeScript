060ebdc1a154b93a0a31ffdb09b1e72e955534a9
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-10-02 18:45:44 +0800

    アサーション関数

--- section00/function03.ts
+++ section00/function03.ts
@@ -9,3 +9,21 @@
 // }
 
 'animal is Duckの部分がType predicateです。これで関数isDuck()がtrueを返す時のifのブロックの中ではanimalはDuck型として解釈されるようになります。'
+
+
+'アサーション関数'
+'ユーザ定義の型ガード関数として使われるのはType predicateがおもですが、Assertion functionという方法もあります。'
+'Type predicateはboolean型の戻り値に対して使ったが、こちらは関数が例外を投げるかどうかで判断します。型ガード関数のページで作った関数isDuck()をAssertion functionsで書き換えると次のようになる'
+// function isDuck(animal: Animal): asserts animal is Duck{
+//     if(walksLikeDuck(animal)){
+//         if(quacksLikeDuck(animal)){
+//             return;
+//         }
+//     }
+//     throw new Error("YOU ARE A FROG!!!");
+// }
+
+// ここではquacks()は存在しない。
+// animal.quacks();
+// isDuck(animal);
+// animal.quacks();

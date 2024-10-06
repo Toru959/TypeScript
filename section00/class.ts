481fda58addfcd1e90a10d29ee4736066b294583
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-10-06 18:34:58 +0800

    field

--- section00/class.ts
+++ section00/class.ts
@@ -32,3 +32,46 @@
 'コンストラクタに引数を渡す'
 'JavaScriptでコンストラクタに引数を渡す方法は、関数呼び出しの書き方とほぼ同じです。違いはnew演算子をつけることです。'
 // new Person("Alice");
+
+'field'
+'JavaScriptでインスタンスにフィールドを持たせるには、インスタンス化したオブジェクトのプロパティに値を代入します。'
+// class Person {}
+// const alice = new Person();
+// alice.name = "Alice";
+
+'TypeScriptでは、これに加えてフィールドの型注釈を書く必要があ必要があります。'
+// class Person {
+//     name: string;
+// }
+// const alice = new Person();
+// alice.name = "Alice";
+
+'フィールドは宣言時に型を省略した場合でも、コンストラクタで値が代入される場合は、代入される値で型が推論されます。'
+// class Person {
+//     private name;
+
+//     constructor(name: string){
+//         this.name = name
+//     }
+// }
+
+'コンストラクタを用いたフィールドの初期化'
+'フィールドへの代入は、コンストラクタを用いて行えます。コンストラクタの中では、thisを用いて値を代入したいフィールドにアクセスします。'
+// class Person{
+//     name: string;
+
+//     constructor(){
+//         this.name = "Alice";
+//     }
+// }
+
+'コンストラクタに引数を持たせれば、フィールドの値を動的に指定できるようにもできます。'
+// class Person {
+//     name: string;
+
+//     constructor(personName: string){
+//         this.name = personName;
+//     }
+// }
+// const alice = new Person("Alice");
+
